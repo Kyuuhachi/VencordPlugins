835bd51c487775b41fd94197db9a1037bd53e957
Kyuuhachi <caagr98@gmail.com>
2024-01-04 22:26:14 +0100

    Add plugin to show links on images

---
+++ 98ImageLink/index.ts
@@ -0,0 +1,17 @@
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "98ImageLink",
+    description: "Suppresses the hiding of links for \"simple embeds\"",
+    authors: [{ id: 236588665420251137n, name: "Kyuuhachi" }],
+
+    patches: [
+        {
+            find: 'isEmbedInline:function',
+            replacement: {
+                match: /(?<=isEmbedInline:function\(\)\{return )\w+(?=\})/,
+                replace: '()=>false',
+            },
+        },
+    ],
+})
