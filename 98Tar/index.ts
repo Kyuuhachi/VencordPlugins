2edebbe9a9bf047ac938626e3b25a22af0451902
Kyuuhachi <caagr98@gmail.com>
2024-01-06 00:28:23 +0100

    Move TarFile to separate file

--- 98Tar/index.ts
+++ 98Tar/index.ts
@@ -1,5 +1,6 @@
 import definePlugin from "@utils/types";
 import { Logger } from "@utils/Logger";
+import TarFile from "./tar";
 
 const logger = new Logger("98Tar", "#CAA698");
 
@@ -105,67 +106,3 @@ async function forceLoadAll(wreq) {
         logger.log(`Loading webpack chunks... (${count}/${ids.length}${errors == 0 ? "" : `, ${errors} errors`})`)
     }));
 }
-
-class TarFile {
-    constructor() {
-        this.buffers = [];
-    }
-
-    addTextFile(name, text, metadata) {
-        this.addFile(name, new TextEncoder().encode(text), metadata);
-    }
-
-    addFile(name, data, {mtime = 0} = {}) {
-        this.buffers.push(this.header([
-            [100, name.toString()], // name
-            [8, 0o644], // mode
-            [8, 0o1000], // uid
-            [8, 0o1000], // gid
-            [12, data.length], // size
-            [12, mtime], // mtime
-            [8, null], // checksum
-            [1, "0"], // type
-            [100, ""], // name of linked file (??)
-            [255, ""], // padding
-        ]));
-        this.buffers.push(data);
-        this.buffers.push(new ArrayBuffer(-data.length & 0x1FF));
-    }
-
-    header(fields) {
-        const buffer = new ArrayBuffer(512);
-        const u1 = new Uint8Array(buffer);
-        let checksum = 0;
-        let checksumPos = null;
-
-        let pos = 0;
-        for(const [size, val] of fields) {
-            let string;
-            if(val === null) {
-                checksumPos = pos;
-                string = " ".repeat(size);
-            } else if(typeof val === "string") {
-                string = val;
-            } else if(typeof val === "number") {
-                string = val.toString(8).padStart(size-1, "0");
-            }
-            if(string.length > size) throw new Error(`${string} is longer than ${size} characters`);
-            Array.from(string).forEach((c, i) => checksum += u1[pos+i] = c.charCodeAt());
-            pos += size;
-        }
-        Array.from("\0".repeat(8)).forEach((c, i) => u1[checksumPos+i] = c.charCodeAt());
-        Array.from(checksum.toString(8).padStart(7, "0")).forEach((c, i) => u1[checksumPos+i] = c.charCodeAt());
-        return buffer;
-    }
-
-    save(filename) {
-        const a = document.createElement("a");
-        a.href = URL.createObjectURL(new Blob(this.buffers, {"type": "application/x-tar"}));
-        a.download = filename;
-        a.style.display = "none";
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
-        URL.revokeObjectURL(a.href);
-    }
-};
---
+++ 98Tar/tar.ts
@@ -0,0 +1,63 @@
+export default class TarFile {
+    constructor() {
+        this.buffers = [];
+    }
+
+    addTextFile(name, text, metadata) {
+        this.addFile(name, new TextEncoder().encode(text), metadata);
+    }
+
+    addFile(name, data, {mtime = 0} = {}) {
+        this.buffers.push(this.header([
+            [100, name.toString()], // name
+            [8, 0o644], // mode
+            [8, 0o1000], // uid
+            [8, 0o1000], // gid
+            [12, data.length], // size
+            [12, mtime], // mtime
+            [8, null], // checksum
+            [1, "0"], // type
+            [100, ""], // name of linked file (??)
+            [255, ""], // padding
+        ]));
+        this.buffers.push(data);
+        this.buffers.push(new ArrayBuffer(-data.length & 0x1FF));
+    }
+
+    header(fields) {
+        const buffer = new ArrayBuffer(512);
+        const u1 = new Uint8Array(buffer);
+        let checksum = 0;
+        let checksumPos = null;
+
+        let pos = 0;
+        for(const [size, val] of fields) {
+            let string;
+            if(val === null) {
+                checksumPos = pos;
+                string = " ".repeat(size);
+            } else if(typeof val === "string") {
+                string = val;
+            } else if(typeof val === "number") {
+                string = val.toString(8).padStart(size-1, "0");
+            }
+            if(string.length > size) throw new Error(`${string} is longer than ${size} characters`);
+            Array.from(string).forEach((c, i) => checksum += u1[pos+i] = c.charCodeAt());
+            pos += size;
+        }
+        Array.from("\0".repeat(8)).forEach((c, i) => u1[checksumPos+i] = c.charCodeAt());
+        Array.from(checksum.toString(8).padStart(7, "0")).forEach((c, i) => u1[checksumPos+i] = c.charCodeAt());
+        return buffer;
+    }
+
+    save(filename) {
+        const a = document.createElement("a");
+        a.href = URL.createObjectURL(new Blob(this.buffers, {"type": "application/x-tar"}));
+        a.download = filename;
+        a.style.display = "none";
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        URL.revokeObjectURL(a.href);
+    }
+};
