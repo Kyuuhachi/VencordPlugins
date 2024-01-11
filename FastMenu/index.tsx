d200b5f25776a7a142a8bfc90e5f818b15fc7367
Kyuuhachi <caagr98@gmail.com>
2024-01-11 23:45:21 +0100

    Add FastMenu

---
+++ FastMenu/index.tsx
@@ -0,0 +1,83 @@
+import { definePluginSettings } from "@api/Settings";
+import { classNameFactory } from "@api/Styles";
+import definePlugin, { OptionType } from "@utils/types";
+import { findByPropsLazy, wreq } from "@webpack";
+import { Forms, useRef } from "@webpack/common";
+
+const cl = classNameFactory("");
+const Classes = findByPropsLazy("animating", "baseLayer", "bg", "layer", "layers");
+
+const settings = definePluginSettings({
+    eagerLoad: {
+        description: "Eagerly load menu contents (faster, but slightly more network load)",
+        type: OptionType.BOOLEAN,
+        default: true,
+        onChange(val) {
+            if(val) eagerLoad();
+        }
+    },
+});
+
+const lazyLayers = [];
+function eagerLoad() {
+    lazyLayers.forEach(wreq.el);
+}
+
+export default definePlugin({
+    name: "FastMenu",
+    description: "Makes the settings menu open faster.",
+    authors: [{ id: 236588665420251137n, name: "Kyuuhachi" }],
+    settings,
+
+    patches: [
+        {
+            find: "this.renderArtisanalHack()",
+            replacement: [
+                { // Fade in on layer
+                    match: /(?<=(\w+)\.contextType=\w+\.AccessibilityPreferencesContext;)/,
+                    replace: "$1=$self.Layer;",
+                },
+                { // Grab lazy-loaded layers
+                    match: /webpackId:("\d+"),name:("\w+")/g,
+                    replace: "$&,_:$self.lazyLayer($1,$2)",
+                },
+            ],
+        },
+        // For some reason standardSidebarView also has a small fade-in
+        {
+            find: "},DefaultCustomContentScroller:function(){return ",
+            replacement: {
+                match: /(?<=Fragment,\{children:)\w+\(\((\w+),\w+\)=>(\(0,\w+\.jsxs\))\(\w+\.animated\.div,\{style:\1,/,
+                replace: "($2(\"div\",{"
+            }
+        }
+    ],
+
+    Layer({ mode, baseLayer = false, ...props }) {
+        const hidden = mode === "HIDDEN";
+        const containerRef = useRef();
+        const node = <div
+            ref={containerRef}
+            aria-hidden={hidden}
+            className={cl({
+                [Classes.layer]: true,
+                [Classes.baseLayer]: baseLayer,
+                "stop-animations": hidden,
+            })}
+            style={{ visibility: hidden ? "hidden" : "visible" }}
+            {...props}
+        />;
+        if(baseLayer) return node;
+        else return <Forms.FocusLock containerRef={containerRef}>{node}</Forms.FocusLock>;
+    },
+
+    lazyLayer(moduleId, name) {
+        if(name !== "CollectiblesShop")
+            lazyLayers.push(moduleId);
+    },
+
+    start() {
+        if(settings.store.eagerLoad)
+            eagerLoad();
+    },
+});
--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ const PLUGINS = [
     require("./Anammox").default,
     require("./Classify").default,
     require("./ColorMessage").default,
+    require("./FastMenu").default,
     require("./HistoryModal").default,
     require("./ImageLink").default,
     require("./ReplyTimestamp").default,
