60c245f0090323e3981c22cd906818788967b707
Kyuuhachi <caagr98@gmail.com>
2024-02-26 21:49:56 +0100

    Configurable Shiggy

--- Shiggy/index.tsx
+++ Shiggy/index.tsx
@@ -1,17 +1,27 @@
-import definePlugin from "@utils/types";
+import { definePluginSettings } from "@api/Settings";
+import definePlugin, { OptionType } from "@utils/types";
+
+const settings = definePluginSettings({
+    shiggy: {
+        description: "Shiggy",
+        type: OptionType.STRING,
+        default: "https://media.discordapp.net/stickers/1039992459209490513.png",
+        restartNeeded: true,
+    }
+});
 
 export default definePlugin({
     name: "Shiggy",
     description: "Shiggy",
     authors: [{ id: 236588665420251137n, name: "Shiggy" }],
-    shiggy: "https://media.discordapp.net/stickers/1039992459209490513.png",
+    settings,
 
     patches: [
         {
             find: "getGuildMemberAvatarURLSimple:function()",
             replacement: {
                 match: /(?<=function (\i)\([^)]*\)\{)(?=.*get\w+URL(?:Simple)?:\1\b)/g,
-                replace: "$&return $self.shiggy;"
+                replace: "$&return $self.settings.store.shiggy;"
             }
         },
     ],
